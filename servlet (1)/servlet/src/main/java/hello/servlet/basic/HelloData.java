cbf02f40ba347cd43e06dc1cd3b7d319f67269d7
segye <tprpek@naver.com>
2023-05-23 18:03:58 +0900

    섹션2 완료

---
+++ servlet (1)/servlet/src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,12 @@
+package hello.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter //lombok
+public class HelloData {
+    private String username;
+    private int age;
+
+
+}
