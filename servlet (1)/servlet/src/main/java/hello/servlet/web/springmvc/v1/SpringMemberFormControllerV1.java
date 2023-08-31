37e6cabfb4818693b51043a8665a894fd9cd421d
wooju <dnwnrnt@naver.com>
2023-09-01 05:14:32 +0900

    8/31

---
+++ servlet (1)/servlet/src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -0,0 +1,16 @@
+package hello.servlet.web.springmvc.v1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+//@Component
+//@RequestMapping
+@Controller // controller annotation 내부에 component 들어가있음 -> 자동으로 스프링 빈으로 등록됨
+public class SpringMemberFormControllerV1 {
+
+    @RequestMapping("/springmvc/v1/members/new-form")
+    public ModelAndView process() {
+        return new ModelAndView("new-form");
+    }
+}
